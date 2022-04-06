1194c54d25890b3ccf715890f47bd47d16e55bed
Tomi <viethung.pham93@gmail.com>
2022-04-06 18:36:21 +0100

    ADD: Created Header

---
+++ src/components/Articles.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Articles() {
+  return (
+    <div className="container mx-auto p-6 pt-16">
+      <h1 className="text-4xl font-bold">Recent articles</h1>
+    </div>
+  );
+}
+
+export default Articles;
